07adb94b347cf8c073a78e8e64102bdd19a606ec
Panwar, Himanshu <himanshu.panwar@boeing.com>
2024-11-25 22:15:46 +0530

    using react state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+
+import { useState } from 'react';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/header/Header.jsx';
 import CoreConcept from './Components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
+
+  const [selectedTopic, setSelectedTopic] = useState('components');
+
   return (
     <div>
       <Header />
@@ -29,14 +35,22 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
   );
 
   function handleSelect(selectedButton) {
-    console.log(selectedButton);
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
   }
 }
 
