9b2fcc7d729bfb38457462bccdda0d7cb0fa0347
Panwar, Himanshu <himanshu.panwar@boeing.com>
2024-11-26 10:25:42 +0530

    splitting components

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,17 @@
-
-import { useState } from 'react';
-import { CORE_CONCEPTS } from './data.js';
 import Header from './components/header/Header.jsx';
-import CoreConcept from './Components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
-import { EXAMPLES } from './data.js';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-
-  const [selectedTopic, setSelectedTopic] = useState('components');
-
   return (
-    <div>
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
-          </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
-
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-    // console.log(selectedTopic);
-  }
 }
 
 export default App;
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types';
 
 TabButton.propTypes = {
     children: PropTypes.string,
-    onSelect: PropTypes.func
+    onSelect: PropTypes.func,
+    isSelected: PropTypes.bool
   };
 
-  export default function TabButton({ children, onSelect }) {
+  export default function TabButton({ children, onSelect, isSelected }) {
     return (
       <li>
-        <button onClick={onSelect}>{children}</button>
+        <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
       </li>
     );
   }
